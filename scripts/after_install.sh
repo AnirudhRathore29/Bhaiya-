231c595affc5d28169c41bb4e437da321c02de88
Anirudh rathore <96767807+AnirudhRathore29@users.noreply.github.com>
2023-05-04 16:40:36 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,11 +3,13 @@
 #_Change_Working_Directory
 cd /home/ubuntu/server
 
+#_Remove_Unused_Code
+rm -rf node_modules
+rm -rf package.json
+rm -rf build
+
 # Install any new dependencies
 npm install
 
 # Build your React application
 npm run build
-
-# Restart your server (if necessary)
-systemctl restart your-server.service
