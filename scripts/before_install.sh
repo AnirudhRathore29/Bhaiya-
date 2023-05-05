0c2f7dbab0e77f6bc949eecf73f2acf28eea3d03
Anirudh rathore <96767807+AnirudhRathore29@users.noreply.github.com>
2023-05-05 17:07:50 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@
 cd /home/ubuntu/server
 
 #_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
 
 #_Download_Node_&NPM
 sudo apt install -y nodejs npm
