a9ac43db2df9ca8639c864279a54c5002b17eb5a
Anirudh rathore <96767807+AnirudhRathore29@users.noreply.github.com>
2023-05-08 16:02:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Pradhumn :-)
+          Edit <code>src/App.js</code> New:-)
         </p>
         <a
           className="App-link"
